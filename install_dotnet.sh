3e85807180b8b0ccf42e5e55b67964a515ae9994
Matthias Löffel <matthias.loeffel@gmail.com>
2023-03-08 16:47:42 +0100

    fix sudo

--- install_dotnet.sh
+++ install_dotnet.sh
@@ -5,4 +5,4 @@ sudo dpkg -i packages-microsoft-prod.deb
 rm packages-microsoft-prod.deb
 
 # this will install dotnet 7 sdk and dotnet/asp.net runtime for 6.0
-sudo apt-get update && apt-get install -y dotnet-sdk-7.0 aspnetcore-runtime-6.0
+sudo apt-get update && sudo apt-get install -y dotnet-sdk-7.0 aspnetcore-runtime-6.0

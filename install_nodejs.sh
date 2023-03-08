ea05854021226d16fbdbf5f3012842792ee53877
Matthias Löffel <matthias.loeffel@gmail.com>
2023-03-08 15:48:51 +0100

    configure npm

--- install_nodejs.sh
+++ install_nodejs.sh
@@ -3,3 +3,8 @@
 sudo apt-get install -y nodejs npm
 sudo npm install -g n
 sudo n lts
+
+# configure npm proxy
+
+npm config set proxy "http://gateway.zscloud.net:10268"
+npm config set strict-ssl false # this is REAL BIG BS

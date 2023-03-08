71f3fa27c144141ff8e6cf3514b521ee18ca665f
Matthias Löffel <matthias.loeffel@gmail.com>
2023-03-08 16:41:40 +0100

    cleanup bootstrap

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -2,16 +2,17 @@ curl (("https://cloud-images.ubuntu.com",
 "wsl/jammy/current",
 "ubuntu-jammy-wsl-amd64-wsl.rootfs.tar.gz") -join "/")  -OutFile rootfs-tar.gz
 
-wsl --set-default-version 2
-wsl --import --import ubuntu-wpaas (($HOME, "ubuntu-wpaas") -join "\") .\rootfs-tar.gz
-
-$key = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Lxss"
 $dist = "ubuntu-wpaas"
+
+wsl --set-default-version 2
+wsl --import --import ubuntu-wpaas (($HOME, $dist) -join "\") .\rootfs-tar.gz
 
 $user = Read-Host -Prompt "Enter your linux username"
 
 wsl -d ubuntu-wpaas -- useradd -u 1000 -m -G sudo -s /bin/bash $user
 wsl -d ubuntu-wpaas -- passwd $user
+
+$key = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Lxss"
 
 Get-ChildItem -Path $key | ForEach-Object {
     $distributionName = (Get-ItemProperty -Path $_.PsPath).DistributionName

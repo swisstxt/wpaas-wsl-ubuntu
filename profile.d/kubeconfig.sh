28342484df9a20b503ad20743d7333bae3fe888b
Matthias Löffel <matthias.loeffel@gmail.com>
2023-03-08 15:04:30 +0100

    add an empty config

--- profile.d/kubeconfig.sh
+++ profile.d/kubeconfig.sh
@@ -1,5 +1,6 @@
 mkdir -p ~/.kube
 touch ~/.kube/config
+touch ~/.kube/empty.config
 export KUBECONFIG=~/.kube/config:$(ls -a ~/.kube/*.config | xargs | sed 's/ /:/g') 
 export NO_PROXY="$NO_PROXY,$(find ~/.kube/ -name '*.config' -exec cat {} \; | grep server: | cut -d: -f3- | cut -d/ -f3- | grep -E swisstxt.ch\|media.int | cut -d: -f1 | sort | uniq |  sed -z 's/\n/,/g')"
 

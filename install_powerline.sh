4539086238a1b5ad3c401e9ce502b1e49f0156a1
Matthias Löffel <matthias.loeffel@gmail.com>
2023-03-10 09:43:23 +0100

    install powerline from git

--- install_powerline.sh
+++ install_powerline.sh
@@ -3,6 +3,5 @@
 read -p "Do you want to install powerline? [y/N] " answer
 
 if [[ "$answer" =~ ^[Yy]$ ]]; then
-    sudo apt install python3-pip
-    pip3 install --user powerline-status
+    sudo apt-get install -y powerline-gitstatus
 fi
--- profile.d/powerline.sh
+++ profile.d/powerline.sh
@@ -1,13 +1,11 @@
 export PATH=$PATH:$HOME/.local/bin
 
-if command -v python3 >/dev/null 2>&1; then 
-    SITE=$(python3 -c "import site; print(site.USER_SITE)")
-fi
+POWERLINE=/usr/share/powerline/bindings/bash/powerline.sh
 
 # Powerline configuration
-if [ -f "$SITE/powerline/bindings/bash/powerline.sh" ]; then
-    $HOME/.local/bin/powerline-daemon -q
+if [ -f "$POWERLINE" ]; then
+    /usr/bin/powerline-daemon -q
     POWERLINE_BASH_CONTINUATION=1
     POWERLINE_BASH_SELECT=1
-    source "$SITE/powerline/bindings/bash/powerline.sh"
+    source "$POWERLINE"
 fi

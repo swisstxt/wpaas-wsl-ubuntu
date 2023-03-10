54df602129a1c840293ce82bc993ed9fab7e80fb
Renato Haeni <renato.haeni@swisstxt.ch>
2023-03-10 10:53:51 +0100

    install dotnet 6 sdk as well and git config

--- install_essentials.sh
+++ install_essentials.sh
@@ -4,3 +4,9 @@ sudo apt-get install -y git build-essential
 # configure git for proxy
 
 git config --global http.proxy http://gateway.zscloud.net:10268
+
+read -p "please enter your email address used as git author:" email
+read -p "please enter your full name used as git author:" name
+
+git config --global user.email "$email"
+git config --global user.name "$name"

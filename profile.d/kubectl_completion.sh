b9bb0e7da6fd3c211fc56750b8304de3e3bf49ec
Matthias Löffel <matthias.loeffel@gmail.com>
2023-03-14 10:33:43 +0100

    kubectl alias + alias completion

--- profile.d/kubectl_completion.sh
+++ profile.d/kubectl_completion.sh
@@ -1,5 +1,7 @@
 if command -v kubectl >/dev/null 2>&1; then
    source <(kubectl completion bash)
+   alias k=kubectl
+   complete -F __start_kubectl k
    export KUBECTL_COMPLETION_INSTALLED=yes
 else
    export KUBECTL_COMPLETION_INSTALLED=no

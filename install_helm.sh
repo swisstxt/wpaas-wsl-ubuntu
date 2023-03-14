01208059d04daa4d85439ed60a1e44a54e42bb0b
Raphael Spanyar <raphael.spanyar@swisstxt.ch>
2023-03-14 13:37:45 +0100

    allow downgrade

--- install_helm.sh
+++ install_helm.sh
@@ -16,4 +16,4 @@ curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share
 sudo apt-get install apt-transport-https --yes
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
 sudo apt-get update
-sudo apt-get install -y helm
+sudo apt-get install -y  --allow-downgrades helm

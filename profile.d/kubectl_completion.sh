7fe7b6114c4a500d40cd65e8a28ccc0134f66b6a
asnapper <matthias.loeffel@gmail.com>
2023-03-14 15:51:40 +0100

    kubectl current-context alias

--- profile.d/kubectl_completion.sh
+++ profile.d/kubectl_completion.sh
@@ -2,6 +2,7 @@ if command -v kubectl >/dev/null 2>&1; then
    source <(kubectl completion bash)
    alias k=kubectl
    complete -F __start_kubectl k
+   alias kc="kubectl config current-context"
    export KUBECTL_COMPLETION_INSTALLED=yes
 else
    export KUBECTL_COMPLETION_INSTALLED=no

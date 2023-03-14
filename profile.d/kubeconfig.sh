4a3f167c6b687c73d22d493c149f2275c424de06
Matthias Löffel <matthias.loeffel@gmail.com>
2023-03-14 10:18:44 +0100

    fix kubeconfig permissions

--- profile.d/kubeconfig.sh
+++ profile.d/kubeconfig.sh
@@ -1,7 +1,14 @@
+#!/bin/bash
+
 mkdir -p ~/.kube
 touch ~/.kube/config
 touch ~/.kube/empty.config
+
+chmod g-r ~/.kube/config
+chmod g-r ~/.kube/*.config
+chmod o-r ~/.kube/config
+chmod o-r ~/.kube/*.config
+
 export KUBECONFIG=~/.kube/config:$(ls -a ~/.kube/*.config | xargs | sed 's/ /:/g') 
 export NO_PROXY="$NO_PROXY,$(find ~/.kube/ -name '*.config' -exec cat {} \; | grep server: | cut -d: -f3- | cut -d/ -f3- | grep -E swisstxt.ch\|media.int | cut -d: -f1 | sort | uniq |  sed -z 's/\n/,/g')"
-
 export KUBECONFIG_INSTALLED=yes

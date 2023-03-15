b1df076cc39d31ee9ac6b2e606e4fb49b0f97cd1
Matthias Löffel <matthias.loeffel@gmail.com>
2023-03-15 18:46:50 +0100

    move post installation tasks into seperate files

--- post_install.sh
+++ post_install.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+# exit on error
+set -ex
 
-sudo nvidia-ctk runtime configure --runtime=docker
-sudo systemctl restart docker
+# import bash function library
+. ./lib.sh
+
+# load http proxy env vars
+. profile.d/http_proxy_env.sh
+
+# post installation for nvidia container runtime
+. ./post_install_nvidia_container_toolkit.sh
---
+++ post_install_nvidia_container_toolkit.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo nvidia-ctk runtime configure --runtime=docker
+sudo systemctl restart docker

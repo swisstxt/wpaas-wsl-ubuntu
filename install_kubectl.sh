b986626ba7675d617bff6f9aa5777714faa64a2b
Roland Fasnacht <135215310+Fasi27@users.noreply.github.com>
2023-06-02 09:18:32 +0200

    Update install_kubectl.sh

--- install_kubectl.sh
+++ install_kubectl.sh
@@ -11,7 +11,8 @@ else
     echo "$KUBECTL_PIN_TARGET is installed and up to date"
 fi
 
-sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /usr/share/keyrings/kubernetes-archive-keyring.gpg
+
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 sudo apt-get update

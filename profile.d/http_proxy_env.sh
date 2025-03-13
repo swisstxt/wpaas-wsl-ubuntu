00475f4f4d222c92e3030f70f229ed8c4fc04d9e
Matthias Löffel <matthias.loeffel@gmail.com>
2025-03-13 11:26:56 +0100

    noproxy for .media.int

--- profile.d/http_proxy_env.sh
+++ profile.d/http_proxy_env.sh
@@ -2,4 +2,4 @@ export HTTPS_PROXY=http://gateway.zscloud.net:10268
 export HTTP_PROXY=http://gateway.zscloud.net:10268
 export https_proxy=http://gateway.zscloud.net:10268
 export http_proxy=http://gateway.zscloud.net:10268
-export NO_PROXY="localhost,.mediahub.ch,.swisstxt.ch,dev-camunda.mediahub.ch,stage-camunda.mediahub.ch,camunda.mediahub.ch,$NO_PROXY"
+export NO_PROXY="localhost,.mediahub.ch,.swisstxt.ch,dev-camunda.mediahub.ch,stage-camunda.mediahub.ch,camunda.mediahub.ch,.media.int,$NO_PROXY"

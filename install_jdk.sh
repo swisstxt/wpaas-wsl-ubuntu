2061c891c3d290897670776177175ee7a3d0ab98
Matthias Löffel <matthias.loeffel@gmail.com>
2025-03-14 09:44:39 +0100

    jdk

--- install_jdk.sh
+++ install_jdk.sh
@@ -5,7 +5,7 @@ sudo mkdir -p /etc/apt/keyrings
 wget -O - https://packages.adoptium.net/artifactory/api/gpg/key/public | sudo tee /etc/apt/keyrings/adoptium.asc
 echo "deb [signed-by=/etc/apt/keyrings/adoptium.asc] https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | sudo tee /etc/apt/sources.list.d/adoptium.list
 sudo apt-get update
-sudo apt-get install -y temurin-17-jdk temurin-11-jdk
+sudo apt-get install -y temurin-17-jdk temurin-11-jdk temurin-23-jdk
 
-default=$(update-alternatives --list java | grep temurin-17)
+default=$(update-alternatives --list java | grep temurin-23)
 sudo update-alternatives --set java $default

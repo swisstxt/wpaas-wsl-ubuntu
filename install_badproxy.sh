4525b7c4add61c19c38033ac2464e5fb352be881
Matthias Löffel <matthias.loeffel@gmail.com>
2025-03-19 10:57:06 +0100

    apt proxy reenable

--- install_badproxy.sh
+++ install_badproxy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# SOURCE=apt/apt.conf.d/99fixbadproxy
-# TARGET=/etc/apt/apt.conf.d/99fixbadproxy
+SOURCE=apt/apt.conf.d/99fixbadproxy
+TARGET=/etc/apt/apt.conf.d/99fixbadproxy
 
-# if does_not_exists_or_is_different $TARGET $SOURCE; then
-#     echo "$TARGET does not appear to be installed - installing"
-#     sudo cp $SOURCE $TARGET
-# else
-#     echo "$TARGET is installed and up to date"
-# fi
+if does_not_exists_or_is_different $TARGET $SOURCE; then
+    echo "$TARGET does not appear to be installed - installing"
+    sudo cp $SOURCE $TARGET
+else
+    echo "$TARGET is installed and up to date"
+fi
